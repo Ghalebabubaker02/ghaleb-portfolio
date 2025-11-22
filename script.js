604fa7e5ed81da62afac3dbcee766514a0807d7f
Ghaleb Rami Abubaker <93747925+Ghalebabubaker02@users.noreply.github.com>
2025-11-23 00:53:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,9 +11,27 @@ document.addEventListener("scroll", () => {
 const showMoreBtn = document.getElementById("showMoreBtn");
 const instaWrapper = document.querySelector(".insta-wrapper");
 const instaGradient = document.querySelector(".insta-gradient");
+const hiddenPosts = document.querySelectorAll(".hidden-posts .instagram-media");
 
 showMoreBtn.addEventListener("click", () => {
+
+    // افتح القسم كامل
     instaWrapper.style.maxHeight = "5000px";
+
+    // شيل الغطاء
     instaGradient.style.opacity = "0";
+
+    // اخفي الزر
     showMoreBtn.style.display = "none";
+
+    // فعّل تحميل الريلات
+    hiddenPosts.forEach(post => {
+        const link = post.getAttribute("data-link");
+        post.setAttribute("data-instgrm-permalink", link);
+    });
+
+    // إعادة تحميل سكربت إنستغرام
+    const script = document.createElement("script");
+    script.src = "//www.instagram.com/embed.js";
+    document.body.appendChild(script);
 });
